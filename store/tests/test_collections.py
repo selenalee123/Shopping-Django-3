9466f4c888c42240b653f6e29504978ca783801e
kim kim <kim.ly2704@gmail.com>
2022-08-22 12:07:52 -0400

    test collection

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -37,8 +37,13 @@ class TestRetrieveCollection:
     def test_if_collection_exists_returns_200(self,api_client):
         collection = baker.make(Collection)
         response = api_client.get(f'/store/collections/{collection.id}/')
-        
+
         assert response.status_code == status.HTTP_200_OK
+        assert response.data =={
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0 
+        }
 
 
     
