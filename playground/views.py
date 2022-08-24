8ff8c286cb20170b8cb1711284fc5cd0bd8aac6d
kim kim <kim.ly2704@gmail.com>
2022-08-24 12:19:23 -0400

    add caching

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,14 @@
+from django.core.cache import cache
 from django.shortcuts import render
-from .tasks import  notify_customers
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
+import requests
 
 
-def say_hello(request):
-    notify_customers.delay('Hello World!')
-    return render(request, 'hello.html', {'name': 'hehe'})
+class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request):
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        return render(request, 'hello.html', {'name': 'Mosh'})
